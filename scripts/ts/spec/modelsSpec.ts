4f5f75cd536d5275a1ef0d6b7eaeca7c37e64473
Samy Dindane <samy@dindane.com>
2013-07-26 01:39:01 +0200

    Add code and tests for ItemsCollection

--- scripts/ts/spec/modelsSpec.ts
+++ scripts/ts/spec/modelsSpec.ts
@@ -22,7 +22,51 @@ describe("Item.constructor()", function() {
   });
 
   it("throws an exception if not logical quantity is specified", function() {
-    // toThrow() doesn't work on the constructor
+    // toThrow() doesn't catch the exceptions
     //expect(new Item("foo", date, -42)).toThrow();
+  });
+});
+
+describe("ItemsCollection: size, add, remove, removeById, getNth", function() {
+  var item0, item1, item2: Item;
+  var items: ItemsCollection;
+
+  it("returns the size of the collection", function() {
+    items = new ItemsCollection();
+    expect(items.size()).toEqual(0);
+
+    item0 = new Item("foo", new Date(), 9000)
+    item1 = new Item("bar", new Date(), 666)
+    items.add(item0);
+    expect(items.size()).toEqual(1);
+
+    items.add(item1);
+    expect(items.size()).toEqual(2);
+
+    item2 = items.getNth(0);
+    expect(item2.name).toEqual("foo");
+
+    // toThrow() doesn't catch the exceptions
+    //expect(items.getNth(100)).toThrow();
+
+    items.remove(item0);
+    item2 = items.getNth(0);
+    expect(item2.name).toEqual("bar");
+    expect(items.size()).toEqual(1);
+
+    items.remove(item1);
+    expect(items.size()).toEqual(0);
+    items.add(item0);
+    items.add(item1);
+    expect(items.size()).toEqual(2);
+
+    items.removeById(item1["id"]);
+    item2 = items.getNth(0);
+    expect(item2.name).toEqual("foo");
+    expect(items.size()).toEqual(1);
+
+    item2 = null;
+    item2 = items.get(item0["id"]);
+    expect(item2.name).toEqual("foo");
   });
 });
--- scripts/ts/src/models.ts
+++ scripts/ts/src/models.ts
@@ -5,7 +5,7 @@ class Item {
   quantity: number;
 
   constructor(name: string, expirationDate: Date, quantity?: number) {
-    this.id = String((new Date()).getTime());
+    this.id = String((new Date()).getTime() + Math.floor(Math.random() * 10000));
     this.name = name;
     this.expirationDate = expirationDate;
 
@@ -16,16 +16,77 @@ class Item {
 }
 
 class ItemsCollection {
-  collection: Item[];
+  private collection: Item[];
+  private _size: number = 0;
 
   constructor() {
     this.collection = [];
   }
 
   add(item: Item) {
+    this._size++;
     this.collection.push(item);
   }
 
-  remove(id: string) {
+  removeById(id: string) {
+    for (var i = 0; i < this._size; i++) {
+      if (this.collection[i]["id"] == id) {
+        this.collection.splice(i, 1);
+        this._size--;
+
+    console.log(this.collection[0])
+    console.log(this.collection[1])
+        return;
+      }
+    }
+  }
+
+  remove(item: Item) {
+    for (var i = 0; i < this._size; i++) {
+      if (this.collection[i]["id"] == item["id"]) {
+        this.collection.splice(i, 1);
+        this._size--;
+        return;
+      }
+    }
+  }
+
+  size(): number {
+    return this._size;
+  }
+
+  getNth(n: number): Item {
+    if (n >= this.size()) throw "Array out of bounds.";
+    return this.collection[n];
+  }
+
+  get(id: string): Item {
+    for (var i = 0; i < this._size; i++) {
+      if (this.collection[i]["id"] == id) {
+        return this.collection[i];
+      }
+    }
+
+    throw "Item not found";
+  }
+}
+
+class ItemsCache {
+  items: { [name :string]: number; };
+
+  constructor() {
+    this.items = {};
+  }
+
+  addItem(name: string, quantity?: number): { [name :string]: number; } {
+    return null;
+  }
+
+  getMostPopularItems(numberOfItems?: number): { [name :string]: number; }[] {
+    return null;
+  }
+
+  clear(): void {
+    this.items = {};
   }
 }
