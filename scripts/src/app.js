e9c3eb4f307069a08a3fa746ce9ddc5752070134
Samy Dindane <samy@dindane.com>
2013-07-27 00:05:03 +0200

    Fix a bug

--- scripts/src/app.js
+++ scripts/src/app.js
@@ -3,7 +3,7 @@ app.models = app.models || {};
 
 $(document).ready(function () {
     app.models.AppStorage.checkForLocalStorage();
-    if (typeof inTestMode === undefined && !inTestMode) {
+    if (!inTestMode) {
         window.mainScreen.init();
         window.mainScreen.render();
     }
