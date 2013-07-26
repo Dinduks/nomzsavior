3ac64a8fe2dfd6c59136f0cb54aaaf8afaf5743d
Samy Dindane <samy@dindane.com>
2013-07-26 16:26:14 +0200

    Clear the form after an add

--- scripts/ts/src/viewport.ts
+++ scripts/ts/src/viewport.ts
@@ -5,16 +5,11 @@ window["viewport"] = {};
 window["items"] = new ItemsCollection();
 
 $(document).ready(function () {
-  var date = new Date();
-  date.setTime(date.getTime() + 60*60*24*1000);
-  $('#date-picker').val(date.toJSON().slice(0,10));
+  setDateToTomorrow();
 
   $("#add-item-btn").on("click", function () {
     window["viewport"].switchPanel()
       .then(function () { $("#title").get(0).focus(); });
-
-    return true;
-  });
 
   $("#submit-btn").on("click", function () {
     var item: Item;
@@ -29,6 +24,21 @@ $(document).ready(function () {
     item = new Item(title, date, quantity);
     window["items"].add(item);
 
+    resetForm();
+
     return false;
   });
 });
+
+function setDateToTomorrow() {
+  var date = new Date();
+  date.setTime(date.getTime() + 60*60*24*1000);
+  $('#date-picker').val(date.toJSON().slice(0,10));
+}
+
+function resetForm() {
+  $("#title").val("");
+  $("#quantity").val("1");
+  setDateToTomorrow();
+  $("#title").get(0).focus();
+}
