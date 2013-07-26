9213a21c02983ae519674abea00179eb3fe8cf49
Jérémy Barbet <jbr@zenexity.com>
2013-07-26 11:20:45 +0200

    And add livereload for html files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,20 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         watch: {
+           options: {
+                forever: true,
+                livereload: true
+            },
             scripts: {
                 files: ['scripts/ts/src/**/*.ts'],
                 tasks: ['typescript'],
                 options: {}
             },
             styles: {
-                files: ['stylesheets/*.css'],
-                options: {
-                    forever: true,
-                    livereload: true
-                }
+                files: ['stylesheets/*.css']
+            },
+            html: {
+                files: ['*.html']
             }
         },
         typescript: {
