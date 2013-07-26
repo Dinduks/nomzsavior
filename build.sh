cc88eb3e98cf49dc63ae2dac23299a9b2ab1f235
Samy Dindane <samy@dindane.com>
2013-07-26 14:31:04 +0200

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-./node_modules/typescript/bin/tsc --out scripts/target/app.js scripts/ts/src/*
-./node_modules/typescript/bin/tsc --out scripts/target/appSpec.js scripts/ts/spec/*
+./node_modules/typescript/bin/tsc --out scripts/target/app.js scripts/ts/src/*.ts
+./node_modules/typescript/bin/tsc --out scripts/target/appSpec.js scripts/ts/spec/*.ts
