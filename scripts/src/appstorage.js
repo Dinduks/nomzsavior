35d8a5c4a8f70c33ec88eb42ad164d394ccb5273
Samy Dindane <samy@dindane.com>
2013-07-28 23:21:08 +0200

    Remove some useless functions

--- scripts/src/appstorage.js
+++ scripts/src/appstorage.js
@@ -1,14 +1,4 @@
 app.models.AppStorage = {
-    getCurrentScreen: function () {
-        return parseInt(localStorage.getItem("currentScreen"), 10);
-    },
-    setCurrentScreen: function (screenNumber) {
-        localStorage.setItem("currentScreen", screenNumber.toString());
-    },
-    swapScreens: function() {
-        if (this.getCurrentScreen() == 1) this.setCurrentScreen(2);
-        else this.setCurrentScreen(1);
-    },
     checkForLocalStorage: function() {
         try {
             localStorage.setItem("enabled", "true");
