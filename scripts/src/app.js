dc3d8a5b68309a6d4cdbd44ad3b957aaed480202
Samy Dindane <samy@dindane.com>
2013-09-19 00:25:57 +0200

    Add GitHub ribbon

--- scripts/src/app.js
+++ scripts/src/app.js
@@ -3,8 +3,17 @@ app.models = app.models || {};
 
 $(document).ready(function () {
     app.models.AppStorage.checkForLocalStorage();
+
     if (!inTestMode) {
         window.mainScreen.init();
         window.mainScreen.render();
+    }
+
+    if (!isMobile()) $("#github-ribbon").show();
+
+    // Source: http://www.tablemaker.net/test/detect-browser.js
+    function isMobile() {
+        var index = navigator.appVersion.indexOf("Mobile");
+        return (index > -1);
     }
 });
